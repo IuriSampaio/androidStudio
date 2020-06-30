ad6a98ee21731f6b09a1ad92c3daa446e25c96ac
Iuri Oliveira Carvalho Sampaio <60737410+IuriSampaio@users.noreply.github.com>
2020-06-30 11:05:38 -0300

    os outros arquivos estão errados, esse ta certo

---
+++ RecyclerView/main/main/java/com/example/reciclerview/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.reciclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.reciclerview.adapter.JogosRecyclerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import com.example.reciclerview.dataSource.dataSource
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val recyclerView = recyclerViewFilmes
+        recyclerView.layoutManager=LinearLayoutManager(applicationContext)
+        recyclerView.adapter=JogosRecyclerAdapter(dataSource.getFilmes())
+    }
+}
